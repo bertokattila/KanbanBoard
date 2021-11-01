ea84a3531a5829b5094c099711952565e0d3e5df
Bertók Attila <bertok.attila99@gmail.com>
2021-11-01 21:25:46 +0100

    ef done

--- server/Program.cs
+++ server/Program.cs
@@ -25,7 +25,10 @@ namespace kanbanboard
 
             KanbanboardRepository repo = new KanbanboardRepository();
             //initData();
-
+            //repo.CardLocationChanged(4, 2, 0);
+            //repo.EditCard(new Card { Id = 2, Title = "Masodikkkkkkkkk taszk", Status = State.Pending, Date = new DateTime(2025, 11, 11), ColumnId = 2 });
+            //repo.DeleteCard(1);
+            //repo.DeleteColumn(2);
             System.Console.WriteLine("{0}", JsonSerializer.Serialize(repo.GetBoard()));
 
         }
--- server/data/KanbanboardRepository.cs
+++ server/data/KanbanboardRepository.cs
@@ -39,21 +39,130 @@ namespace kanbanboard
         }
         public void EditCard(Card card)
         {
-            throw new NotImplementedException();
+            using (var db = createDbContext())
+            {
+                var cardToModify = db.Cards
+                                .Where(c => c.Id == card.Id)
+                                .FirstOrDefault();
+
+                if (cardToModify != null)
+                {
+                    cardToModify.Title = card.Title;
+                    cardToModify.Description = card.Description;
+                    cardToModify.Status = card.Status;
+                    cardToModify.Date = card.Date;
+
+                    db.SaveChanges();
+                }
+            }
         }
         public void DeleteCard(int cardId)
         {
-            throw new NotImplementedException();
+            using (var db = createDbContext())
+            {
+                var cardToDelete = db.Cards
+                                    .Where(c => c.Id == cardId)
+                                    .SingleOrDefault();
+
+                if (cardToDelete != null)
+                {
+                    var cardsToShift = db.Cards
+                                        .Where(card => card.ColumnId == cardToDelete.ColumnId && card.Position > cardToDelete.Position);
+
+                    foreach (var card in cardsToShift)
+                    {
+                        card.Position--;
+                    }
+                    db.Remove(cardToDelete);
+                    db.SaveChanges();
+                }
+            }
         }
 
         public void DeleteColumn(int columnId)
         {
-            throw new NotImplementedException();
+            using (var db = createDbContext())
+            {
+                var columnToDelete = db.Columns
+                                    .Where(c => c.Id == columnId)
+                                    .SingleOrDefault();
+
+                if (columnToDelete != null)
+                {
+                    var cardsToDelete = db.Cards
+                                        .Where(card => card.ColumnId == columnToDelete.Id);
+
+                    foreach (var card in cardsToDelete)
+                    {
+                        db.Remove(card);
+                    }
+                    db.Remove(columnToDelete);
+                    db.SaveChanges();
+                }
+            }
         }
 
-        public void CardLocationChanged(int cardid, int newColId, int newPos)
+        public void CardLocationChanged(int cardId, int newColId, int newPos)
         {
-            throw new NotImplementedException();
+            using (var db = createDbContext())
+            {
+                var card = db.Cards
+                            .Where(card => card.Id == cardId)
+                            .SingleOrDefault();
+                if (card != null)
+                {
+                    if (card.ColumnId == newColId)
+                    {
+                        int oldPos = card.Position;
+                        if (oldPos == newPos) return;
+                        if (newPos > oldPos)
+                        { // cards between shuld shift to lower positions
+                            var cardsToShift = db.Cards
+                                                .Where(c => c.ColumnId == card.ColumnId && c.Position >= oldPos && c.Position <= newPos);
+
+                            foreach (var cardToShift in cardsToShift)
+                            {
+                                cardToShift.Position--;
+                            }
+                        }
+                        else
+                        { // cards between shuld shift to higher positions
+                            var cardsToShift = db.Cards
+                                                .Where(c => c.ColumnId == card.ColumnId && c.Position <= oldPos && c.Position >= newPos);
+
+                            foreach (var cardToShift in cardsToShift)
+                            {
+                                cardToShift.Position++;
+                            }
+                        }
+                    }
+                    else // moves to new col
+                    {
+                        /// old col adjust
+                        var cardsToShiftOldCol = db.Cards
+                                            .Where(c => c.ColumnId == card.ColumnId && c.Position >= card.Position);
+
+                        foreach (var cardToShift in cardsToShiftOldCol)
+                        {
+                            cardToShift.Position--;
+                        }
+
+                        /// new col adjust
+                        var cardsToShiftNewCol = db.Cards
+                                            .Where(c => c.ColumnId == newColId && c.Position >= newPos);
+
+                        foreach (var cardToShift in cardsToShiftNewCol)
+                        {
+                            cardToShift.Position++;
+                        }
+
+                        card.ColumnId = newColId;
+                    }
+
+                    card.Position = newPos;
+                    db.SaveChanges();
+                }
+            }
         }
         public List<Object> GetBoard()
         {
